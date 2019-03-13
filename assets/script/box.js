b8f4fe5e57cf373893015e7a259c9951bc34ac01
TeddyTsui <teddy_tsui@163.com>
2019-03-13 18:42:33 +0800

    add box status

--- assets/script/box.js
+++ assets/script/box.js
@@ -1,16 +1,60 @@
+let Status = cc.Enum({
+    ready: 1,
+    building: 2,
+    built: 3,
+    base: 4,
+})
+
 cc.Class({
     extends: cc.Component,
 
     properties: {
-        
+        _status: {
+            default: Status.ready,
+            type: Status,
+            visible: false,
+        },
+        status: {
+            type: Status,
+            get() {
+                return this._status
+            },
+            set(value) {
+                this._status = value
+                switch (value){
+                    case Status.building :
+                        this.node.getComponent(cc.PrismaticJoint).enabled = false
+                        this.node.getComponent(cc.RigidBody).fixedRotation = true
+                        cc.log(value)
+                        //TODO set speed
+                        break
+                    case Status.built :
+                        this.node.getComponent(cc.RigidBody).fixedRotation = false
+                        this.node.getComponent(cc.RevoluteJoint).enabled = true
+                        cc.log(value)
+                        break
+                    case Status.base :
+                        this.node.getComponent(cc.RigidBody).type = cc.RigidBodyType.Static
+                        cc.log(value)
+                        break
+                    default: 
+                        cc.log(value)
+                }
+            }
+        }
     },
 
     onLoad() {
-
-    },
     
+    },
+
     onTouch() {
         cc.log('emit func onTouch')
         cc.log(this.getComponent(cc.RopeJoint).enabled)
+    },
+
+    wave() {
+        //TODO 摇摆
     }
+
 })
