680e5aa055eca0d3178ea1f33e307246b44714be
TeddyTsui <teddy_tsui@163.com>
2019-03-18 18:05:12 +0800

    try some code

--- assets/script/box.js
+++ assets/script/box.js
@@ -19,25 +19,24 @@ cc.Class({
 
         isTop: false,
 
-        rotate_angle: {
+        rotate_angle: {// 基座旋转角度
             default: 0,
             type: cc.Integer
         },
 
-        status: {
+        status: { // 状态
             type: Status,
             get() {
                 return this._status
             },
             set(value) {
-                cc.log('status changed')
+                cc.log('status changed: ' + value)
                 this._status = value
                 switch (value){
                     case Status.building :
                         this.node.getComponent(cc.PrismaticJoint).enabled = false
                         this.node.getComponent(cc.RigidBody).fixedRotation = true
                         this.node.getComponent(cc.RigidBody).linearVelocity = cc.v2(0, 0)
-                        //TODO set speed
                         break
                     case Status.built :
                         this.node.getComponent(cc.RigidBody).fixedRotation = false
@@ -63,24 +62,49 @@ cc.Class({
         }
     },
 
-    onTouch() {
-        cc.log('emit func onTouch')
-        cc.log(this.getComponent(cc.RopeJoint).enabled)
-    },
-
     onBeginContact: function (contact, selfCollider, otherCollider) {
         if(this.status == Status.building){
-            if(otherCollider.node.isTop){
+            if(otherCollider.node.getComponent('box').isTop){
+                // TODO 判断落点(接触点)
+
+                let selfPos = this.node.convertToWorldSpaceAR(cc.v2(0, 0))
+                let otherPos = otherCollider.node.convertToWorldSpaceAR(cc.v2(0, 0))
+
+                cc.log( 'onBeginContact say sPos: ' + selfPos + ' oPos: ' + otherPos )
+
                 let currentRoof = otherCollider.node
                 let joint = currentRoof.getComponent(cc.RevoluteJoint)
                 joint.connectedBody = this.node.getComponent(cc.RigidBody)
-                this.node.status = Status.built
+                joint.connectedAnchor = cc.v2(0, -50)
+                joint.anchor = cc.v2(0, 50)
+                this.status = Status.built
+            }
+        }
+    },
+
+    onCollisionEnter: function (other, self) {
+        if(this.status == Status.building){
+            if(otherCollider.node.getComponent('box').isTop){
+                // TODO 判断落点(接触点)
+
+                let selfPos = this.node.convertToWorldSpaceAR(cc.v2(0, 0))
+                let otherPos = otherCollider.node.convertToWorldSpaceAR(cc.v2(0, 0))
+
+                cc.log( 'onCollisionEnter say sPos: ' + selfPos + ' oPos: ' + otherPos )
+                
+                let currentRoof = otherCollider.node
+                let joint = currentRoof.getComponent(cc.RevoluteJoint)
+                // this.node.getComponent(cc.RigidBody)
+                joint.connectedBody = this.node.getComponent(cc.RigidBody)
+                joint.connectedAnchor = cc.v2(0, -50)
+                joint.anchor = cc.v2(0, 50)
+                this.status = Status.built
             }
         }
     },
 
     wave() {
-        //TODO 摇摆
+        // 基座摇摆
         let waveSequence = cc.sequence(cc.rotateTo(1, 15),cc.rotateTo(1, 0), cc.rotateTo(1, -15), cc.rotateTo(1, 0))
         let action = cc.repeatForever(waveSequence)
         this.node.runAction(action)
--- assets/script/settings.js
+++ assets/script/settings.js
@@ -1,11 +1,16 @@
 cc.game.on(cc.game.EVENT_ENGINE_INITED, () => {
-    let physicsManager = cc.director.getPhysicsManager();
-    physicsManager.enabled = true;
+    
+    let physicsManager = cc.director.getPhysicsManager()
+    
+    physicsManager.enabled = true
     
     physicsManager.debugDrawFlags = 
         // 0;
         // cc.PhysicsManager.DrawBits.e_aabbBit |
         cc.PhysicsManager.DrawBits.e_jointBit |
         cc.PhysicsManager.DrawBits.e_shapeBit
-        ;
+
+    // let colliderManager = cc.director.getCollisionManager()
+
+    // colliderManager.enabled = true
 });
